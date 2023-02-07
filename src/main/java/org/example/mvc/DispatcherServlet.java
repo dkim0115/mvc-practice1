0cb8bc7ebaf408bee5f6cf1fff465d1100c9d9b6
pawerman <104953295+pawerman@users.noreply.github.com>
2023-02-07 18:43:32 -0500

    first mod

--- src/main/java/org/example/mvc/DispatcherServlet.java
+++ src/main/java/org/example/mvc/DispatcherServlet.java
@@ -1,16 +1,60 @@
 package org.example.mvc;
 
+import org.example.mvc.controller.Controller;
+import org.example.mvc.controller.RequestMethod;
+import org.example.mvc.view.JspViewResolver;
+import org.example.mvc.view.View;
+import org.example.mvc.view.ViewResolver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+
 
 @WebServlet("/")
 public class DispatcherServlet extends HttpServlet {
     private static final Logger log = LoggerFactory.getLogger(DispatcherServlet.class);
 
+    private RequestMappingHandlerMapping rmhm;
+
+    private List<ViewResolver> viewResolvers;
+
+    @Override
+    public void init() throws ServletException{
+        rmhm = new RequestMappingHandlerMapping();
+        rmhm.init();
+
+        viewResolvers = Collections.singletonList(new JspViewResolver());
+    }
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+        log.info("[DispatcherServlet] service started.");
+
+        try {
+            Controller handler = rmhm.findHandler(new HandlerKey(RequestMethod.valueOf(request.getMethod()),request.getRequestURI()));
+            String viewName = handler.handleRequest(request, response);
 
 
+            for (ViewResolver viewResolver : viewResolvers) {
+                View view = viewResolver.resolveView(viewName);
+                view.render(new HashMap<>(),request,response);
+
+            }
+//            RequestDispatcher requestDispatcher = request.getRequestDispatcher(viewName);
+//            requestDispatcher.forward(request, response);
+        } catch (Exception e) {
+            log.error("exception occured: [{}]", e.getMessage(),e);
+        }
+    }
 
 }
