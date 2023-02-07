4b328cd220e520aec83cababf38c47a129ad93a1
bookck13 <bookck13@gmail.com>
2023-02-07 12:30:46 -0500

    Initial commit

---
+++ src/main/java/org/example/mvc/DispatcherServlet.java
@@ -0,0 +1,16 @@
+package org.example.mvc;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet("/")
+public class DispatcherServlet extends HttpServlet {
+    private static final Logger log = LoggerFactory.getLogger(DispatcherServlet.class);
+
+
+
+
+}
